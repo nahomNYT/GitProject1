7f239855d9aacf4a4ca38fa76dd1639495005342
nahomNYT <140636945+nahomNYT@users.noreply.github.com>
2023-07-29 09:49:37 -0500

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -14,6 +14,10 @@ public class DemoDay2 {
 	public static void subtract(int a, int b) {
 		System.out.println(a - b);
 	}
+
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 	
 
 }
